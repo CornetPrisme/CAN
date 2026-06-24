d2938c3db1d9039ef9b638bb9e1b00e094aee22f
Blain <victor.blain2006@gmail.com>
2026-06-24 08:49:58 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,14 @@
 
 CanManager can;
 
-CAN_STRUCT(TEMPERATURE, 106, 
-  int32_t temp;
+CAN_STRUCT(BOUTTON, 106, 
+  bool  boutton1;
 );
-
-TEMPERATURE msg;
-
-void envoie(can_frame_t frameRecue) {
-    can.send(msg);
-    msg.temp += 1; 
-    delay(1000);
-}
-
-void reception(can_frame_t frameRecue) {
-    while (can.receive_can(&frameRecue)) {
-        
-        if (frameRecue.id == TEMPERATURE::ID) {
-            TEMPERATURE msg_recu;
-            memcpy(&msg_recu, frameRecue.data, sizeof(TEMPERATURE));
-
-            Serial.print("Message ID 100 reçu : Temp = ");
-            Serial.println(msg_recu.temp);
-        }
-    }
-}
 
 
 
 void setup() {
     Serial.begin(115200);
-
     #ifdef ARDUINO_ARCH_ESP32
       can.init(GPIO_NUM_11, GPIO_NUM_12);
     #endif
@@ -39,10 +17,8 @@ void setup() {
       can.init();
     #endif
     
-    msg.temp = 0;
+
 }
 void loop() {
-    can_frame_t frameRecue;
-    //envoie(frameRecue);
-    reception(frameRecue);
+    can.update();
 }
