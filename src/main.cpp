66023f5e12a9aa8b8e89aaf9f0eadd310da04331
Blain <victor.blain2006@gmail.com>
2026-08-21 22:01:10 +0200

    test moteur

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
 #include "CanMessages.h"
 
+/*
 void setup() {
 }
 
 
 void loop() {
 }
+*/
 
-/*
 CanManager can;
 
 unsigned long lastSendTime = 0;
 const unsigned long sendInterval = 2000;
 
-bool etatMoteur = false;
+uint8_t vitesseMoteur = 50;
 
 void setup() {
     Serial.begin(115200);
@@ -31,14 +32,31 @@ void setup() {
 void loop() {
     can.update();
 
+    if (Serial.available() > 0) {
+        int input = Serial.parseInt();
+        
+        if (input >= 0 && input <= 255) {
+            vitesseMoteur = input;
+            Serial.printf("\n>>> Nouvelle vitesse enregistree : %d <<<\n\n", vitesseMoteur);
+        }
+        
+
+        while(Serial.available() > 0) {
+            Serial.read();
+        }
+    }
+
     if (millis() - lastSendTime >= sendInterval) {
       lastSendTime = millis();
-      etatMoteur = !etatMoteur;
 
-      MOTEUR moteurMsg;
-      moteurMsg.etat = etatMoteur;
-      Serial.println("Message envoyé");
-      Serial.println(etatMoteur ? "Marche (1)" : "Arrêt (0)");
+      MOTEUR msg;
+      msg.motor_id = motor_id_t::RIGHT;
+      msg.etat = true;
+      msg.speed = vitesseMoteur;
+      can.send(msg);
+      
+      Serial.print("Message CAN envoye -> ");
+      Serial.print(" | Vitesse : ");
+      Serial.println(vitesseMoteur);
     }
 }
-*/
