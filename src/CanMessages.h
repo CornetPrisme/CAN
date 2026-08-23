85bb2568eda0a118a892f771dc7c03f496b40ca1
Victor Blain <victor.blain2006@gmail.com>
2026-08-23 18:03:03 +0200

    Maj STRUCT MOTOR

--- src/CanMessages.h
+++ src/CanMessages.h
@@ -4,10 +4,12 @@
 enum motor_id_t: uint8_t {
   RIGHT,
   LEFT,
+  BOTH,
 };
 
-CAN_STRUCT(MOTEUR, 200,
+CAN_STRUCT(MOTOR, 200,
   motor_id_t motor_id;
   bool  etat;
+  bool forward;
   uint8_t speed;
 );
